ce62a7ba64f4411a1560f44a0990deeccb8146ff
design <a0104design@gmail.com>
2026-05-13 15:57:01 +0900

    SignUpPage 디자인 완성

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -4,7 +4,12 @@ import type { ButtonHTMLAttributes, ElementType, ReactNode } from "react";
 export type ButtonColorType = "primary" | "secondary" | "success" | "error" | "warning" | "info";
 export type ButtonVariantType = "contained" | "text" | "icon";
 
-const StyledButton = styled.button<{ $color: ButtonColorType; $variant: ButtonVariantType }>`
+const StyledButton = styled.button<{
+    $color: ButtonColorType;
+    $variant: ButtonVariantType;
+    fullWidth?: boolean;
+}>`
+    width: ${props => (props.fullWidth ? "100%" : "auto")};
     font-size: 14px;
     font-weight: 600;
     color: ${props => (props.$variant === "contained" ? "#ffffff" : "inherit")};
@@ -25,17 +30,18 @@ const StyledButton = styled.button<{ $color: ButtonColorType; $variant: ButtonVa
 // Button태그의 타입 :  ButtonHTMLAttributes<HTMLButtonElement>
 
 //즉, 우리가 만든 Button 컴포포넌트는 button의 확장판이다.
- interface Props  extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     color: ButtonColorType;
     variant: ButtonVariantType;
-    as?: ElementType,
+    as?: ElementType;
     to?: string;
+    fullWidth?: boolean;
 }
 
-function Button({ children, color, variant, ...props }: Props) {
+function Button({ children, color, variant, fullWidth, ...props }: Props) {
     return (
-        <StyledButton $color={color} $variant={variant} {...props as any}>
+        <StyledButton $color={color} $variant={variant} fullWidth={fullWidth} {...(props as any)}>
             {children}
         </StyledButton>
     );
--- src/schemas/auth/signUpSchema.ts
+++ src/schemas/auth/signUpSchema.ts
@@ -12,10 +12,10 @@ export const signUpSchema = z.object({
     password: z.string().min(6, "비밀번호는 6자 이상이여햐 합니다"),
     passwordConfirm: z.string().min(1, "비밀번호 확인을 입력해 주세요"),
     name: z.string().min(2 , "이름을 정확히 입력해주세요"),
-    nickName: z.string().min(2, "닉네임은 2자 이상 입력해주세요").max(10, "닉네임은 10자 이하로 입력해주세요"),
+    nickname: z.string().min(2, "닉네임은 2자 이상 입력해주세요").max(10, "닉네임은 10자 이하로 입력해주세요"),
     email: z.email("올바른 이메일 형식이 아닙니다"),
     phoneNumber: z.string().regex(phoneRegex).optional(),
-    birthday: z.string().optional(),
+    birthdate: z.string().optional(),
     gender: z.enum(Gender),
 })
     .refine(data => data.password === data.passwordConfirm, {
